d29d04df1736fafb90b06b8c153ada975f2125b1
ndrplz <ndrplz@gmail.com>
2017-06-05 14:38:14 +0200

    Add integration with TF checkpoints
    *Pretrained model is now loaded from pickle weights and then saved as TF checkpoint
     using tf.train.Saver class. In this way the model can be imported from different
     projects almost without dependencies (i.e. both graph and weights are contained
     in the checkpoint)

--- main_tf.py
+++ main_tf.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import pickle
 import cv2
+import os
 import os.path as path
 from utils import predict
 from model import dilation_model_pretrained
@@ -10,7 +11,7 @@ from datasets import CONFIG
 if __name__ == '__main__':
 
     # Choose between 'cityscapes' and 'camvid'
-    dataset = 'camvid'
+    dataset = 'cityscapes'
 
     # Load dict of pretrained weights
     print('Loading pre-trained weights...')
@@ -18,28 +19,49 @@ if __name__ == '__main__':
         w_pretrained = pickle.load(f)
     print('Done.')
 
-    # Choose input shape according to dataset characteristics
-    input_h, input_w, input_c = CONFIG[dataset]['input_shape']
-    input_tensor = tf.placeholder(tf.float32, shape=(None, input_h, input_w, input_c))
+    # Create checkpoint directory
+    checkpoint_dir = path.join('data/checkpoint', 'dilation_' + dataset)
+    if not path.exists(checkpoint_dir):
+        os.makedirs(checkpoint_dir)
 
-    # Create pretrained model
-    model = dilation_model_pretrained(dataset, input_tensor, w_pretrained, trainable=False)
+    # Image in / out parameters
+    input_image_path  = path.join('data', dataset + '.png')
+    output_image_path = path.join('data', dataset + '_out.png')
 
-    # Test pretrained model
+    # Build pretrained model and save it as TF checkpoint
     with tf.Session() as sess:
+
+        # Choose input shape according to dataset characteristics
+        input_h, input_w, input_c = CONFIG[dataset]['input_shape']
+        input_tensor = tf.placeholder(tf.float32, shape=(None, input_h, input_w, input_c), name='input_placeholder')
+
+        # Create pretrained model
+        model = dilation_model_pretrained(dataset, input_tensor, w_pretrained, trainable=False)
 
         sess.run(tf.global_variables_initializer())
 
-        # Parameters
-        input_image_path  = path.join('data', dataset+'.png')
-        output_image_path = path.join('data', dataset+'_out.png')
+        # Save both graph and weights
+        saver = tf.train.Saver(tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES))
+        saver.save(sess, path.join(checkpoint_dir, 'dilation'))
+
+    # Restore both graph and weights from TF checkpoint
+    with tf.Session() as sess:
+
+        saver = tf.train.import_meta_graph(path.join(checkpoint_dir, 'dilation.meta'))
+        saver.restore(sess, tf.train.latest_checkpoint(checkpoint_dir))
+
+        graph = tf.get_default_graph()
+        model = graph.get_tensor_by_name('softmax:0')
+        model = tf.reshape(model, shape=(1,)+CONFIG[dataset]['output_shape'])
 
         # Read and predict on a test image
         input_image = cv2.imread(input_image_path)
+        input_tensor = graph.get_tensor_by_name('input_placeholder:0')
         predicted_image = predict(input_image, input_tensor, model, dataset, sess)
 
         # Convert colorspace (palette is in RGB) and save prediction result
         predicted_image = cv2.cvtColor(predicted_image, cv2.COLOR_BGR2RGB)
         cv2.imwrite(output_image_path, predicted_image)
+
 
 
